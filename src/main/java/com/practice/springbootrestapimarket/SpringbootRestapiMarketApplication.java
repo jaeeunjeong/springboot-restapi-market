d601dc49e79c4eb391e4ee62c13ac182696e6b4a
jaeeunjeong <jejeong000@gmail.com>
2022-04-19 09:30:25 +0900

    init.

---
+++ src/main/java/com/practice/springbootrestapimarket/SpringbootRestapiMarketApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springbootrestapimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestapiMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestapiMarketApplication.class, args);
+	}
+
+}

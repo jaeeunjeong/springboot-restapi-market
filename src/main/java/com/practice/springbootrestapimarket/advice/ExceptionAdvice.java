fee0ba86422062b8b6fe558b94db2e78ba86fe32
jaeeunjeong <jejeong000@gmail.com>
2022-05-13 20:17:40 +0900

    Add : 예외 처리 기능 추가

---
+++ src/main/java/com/practice/springbootrestapimarket/advice/ExceptionAdvice.java
@@ -0,0 +1,59 @@
+package com.practice.springbootrestapimarket.advice;
+
+import com.practice.springbootrestapimarket.dto.response.Response;
+import com.practice.springbootrestapimarket.exception.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionAdvice {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Response exception(Exception e) {
+        log.info("e = {}", e.getMessage());
+        return Response.failure(-1000, "오류가 발생하였습니다.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return Response.failure(-1003, e.getBindingResult().getFieldError().getDefaultMessage());
+    }
+
+    @ExceptionHandler(LoginFailureException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public Response loginFailureException() {
+        return Response.failure(-1004, "로그인에 실패하였습니다.");
+    }
+
+    @ExceptionHandler(MemberEmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response memberEmailAlreadyExistsException(String email) {
+
+        return Response.failure(-1005, "중복된 이메일입니다.");
+    }
+
+    @ExceptionHandler(MemberNicknameAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public Response memberNicknameAlreadyExistsException(String email) {
+        return Response.failure(-1006, "중복된 닉네임입니다.");
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response memberNotFoundException() {
+        return Response.failure(-1007, "요청한 회원을 찾을 수 없습니다.");
+    }
+
+    @ExceptionHandler(RoleNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response roleNotFoundException() {
+        return Response.failure(-1008, "요청한 권한을 찾을 수 없습니다.");
+    }
+}

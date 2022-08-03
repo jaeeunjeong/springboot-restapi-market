19b231a0a38ef65dc935856373b86403d6272ba0
jaeeunjeong <jejeong000@gmail.com>
2022-08-03 22:20:08 +0900

    add : 게시판 엔티티 설계

---
+++ src/main/java/com/practice/springbootrestapimarket/entity/post/Post.java
@@ -0,0 +1,59 @@
+package com.practice.springbootrestapimarket.entity.post;
+
+import com.practice.springbootrestapimarket.entity.category.Category;
+import com.practice.springbootrestapimarket.entity.common.EntityDate;
+import com.practice.springbootrestapimarket.entity.member.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends EntityDate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    @Lob
+    private Long price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Category category;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private List<Image> images;
+
+    public Post(String title, Long price, Member member, Category category, List<Image> images) {
+        this.title = title;
+        this.price = price;
+        this.member = member;
+        this.category = category;
+        this.images = images;
+        addImages(images);
+    }
+
+    private void addImages(List<Image> added) {
+        added.stream().forEach(i -> {
+            images.add(i);
+            i.initPost(this);
+        });
+    }
+}

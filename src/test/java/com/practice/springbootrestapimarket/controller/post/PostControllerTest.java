8ac87f404a289f015050c1992e2f91e40d873dfa
jaeeunjeong <jejeong000@gmail.com>
2022-08-21 19:50:56 +0900

    fix : 이미지를 가져와서 저장할 수 있는 경우로 다시 테스트

--- src/test/java/com/practice/springbootrestapimarket/controller/post/PostControllerTest.java
+++ src/test/java/com/practice/springbootrestapimarket/controller/post/PostControllerTest.java
@@ -1,22 +1,28 @@
 package com.practice.springbootrestapimarket.controller.post;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.springbootrestapimarket.dto.post.PostCreateRequest;
 import com.practice.springbootrestapimarket.service.post.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.multipart.MultipartFile;
 
-import static com.practice.springbootrestapimarket.factory.dto.PostCreateRequestFactory.postCreateRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.practice.springbootrestapimarket.factory.dto.PostCreateRequestFactory.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,7 +33,6 @@ class PostControllerTest {
     @Mock
     PostService postService;
     MockMvc mockMvc;
-    ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void beforeEach() {
@@ -38,14 +43,30 @@ class PostControllerTest {
     @Test
     void test1() throws Exception {
         // given
-        PostCreateRequest req = postCreateRequest();
+        ArgumentCaptor<PostCreateRequest> postCreateRequestArgumentCaptor = ArgumentCaptor.forClass(PostCreateRequest.class);
+        List<MultipartFile> images = new ArrayList<>();
+        images.add(new MockMultipartFile("test1", "test1.PNG", MediaType.IMAGE_PNG_VALUE, "image1".getBytes()));
+        images.add(new MockMultipartFile("test2", "test2.PNG", MediaType.IMAGE_PNG_VALUE, "image2".getBytes()));
+        PostCreateRequest req = postCreateRequestWithImages(images);
 
         // when, then
         mockMvc.perform(
-                post("/api/posts")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(req)))
-                .andExpect(status().isOk());
+                multipart("/api/posts")
+                        .file("images", images.get(0).getBytes())
+                        .file("images", images.get(1).getBytes())
+                        .param("title", req.getTitle())
+                        .param("content", req.getContent())
+                        .param("price", String.valueOf(req.getPrice()))
+                        .param("categoryId", String.valueOf(req.getCategoryId()))
+                        .with(requestPostProcessor -> {
+                            requestPostProcessor.setMethod("POST");
+                            return requestPostProcessor;
+                        })
+                        .contentType(MediaType.MULTIPART_FORM_DATA))
+                .andExpect(status().isCreated());
+
         verify(postService).create(req);
+        PostCreateRequest createRequest = postCreateRequestArgumentCaptor.capture();
+        assertThat(createRequest.getImages().size()).isEqualTo(2);
     }
 }
